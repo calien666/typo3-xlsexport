7b1513aacb4ffff50642fe87298a2b65bf2a461a
Luat Dinh Bui <luat@web-vision.de>
2026-02-23 17:14:34 +0100

    [TASK] Update TYPO3 Version requirement & Remove ext_tables.php file.
    - Change minimum required version on TYPO3 pacakges from 12.0 to 12.4
    - Remove Unnecesary file `ext_tables.php`

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '3.1.8',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.0.0-12.4.99',
+            'typo3' => '12.4.0-12.4.99',
         ],
         'conflicts' => [
         ],
